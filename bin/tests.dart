243025c69e91827b8aa54b61d36ee6069ef612ca
Varun Resh Maddali <mvresh@gmail.com>
2019-11-20 16:55:36 +0530

    wrapped the finalList and flattenList function in another function.

--- bin/tests.dart
+++ bin/tests.dart
@@ -3,18 +3,18 @@ import 'main.dart';
 
 void main() {
   test('test 1', () {
-    expect(flattenList([1, "2",() { return 4; }, [3, 'hello', [ "five" ], "six", true, { 'prop': "val" }]]), [1, '2', 4, 3, 'hello', 'five', 'six', true, {'prop': 'val'}]);
+    expect(holisticFlattenList([1, "2",() { return [4]; }, [3, 'hello', [ "five" ], "six", true, { 'prop': "val" }]]), [1, '2', 4, 3, 'hello', 'five', 'six', true, {'prop': 'val'}]);
   });
 
-  test('test 2', () {finalList.clear();
-    expect(flattenList([1, "2",() { return 'how are you'; }, [3, 'hello', [ "five" ], "six", true, { 'prop': "val" }]]), [1, '2', 'how are you', 3, 'hello', 'five', 'six', true, {'prop': 'val'}]);
+  test('test 2', () {
+    expect(holisticFlattenList([1, "2",() { return 'how are you'; }, [3, 'hello', [ "five" ], "six", true, { 'prop': "val" }]]), [1, '2', 'how are you', 3, 'hello', 'five', 'six', true, {'prop': 'val'}]);
   });
 
-  test('test 3', () {finalList.clear();
-    expect(flattenList([1, "2",() { return (4+8); }, [3, 'hello', [ "five" ], "six", true, { 'prop': "val" }]]), [1, '2', 12, 3, 'hello', 'five', 'six', true, {'prop': 'val'}]);
+  test('test 3', () {
+    expect(holisticFlattenList([1, "2",() { return (4+8); }, [3, 'hello', [ "five" ], "six", true, { 'prop': "val" }]]), [1, '2', 12, 3, 'hello', 'five', 'six', true, {'prop': 'val'}]);
   });
 
-  test('test 4', () {finalList.clear();
-    expect(flattenList([1, "2",() { return [1,3,5,7,9]; }, [3, 'hello', [ "five" ], "six", true, { 'prop': "val" }]]), [1, '2', 1,3,5,7,9, 3, 'hello', 'five', 'six', true, {'prop': 'val'}]);
+  test('test 4', () {
+    expect(holisticFlattenList([1, "2",() { return [1,3,5,7,9]; }, [3, 'hello', [ "five" ], "six", true, { 'prop': "val" }]]), [1, '2', 1,3,5,7,9, 3, 'hello', 'five', 'six', true, {'prop': 'val'}]);
   });
 }
